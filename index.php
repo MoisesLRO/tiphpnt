201ca152c785d6395901a67dbfdd3b1efa0a3118
MoisesLRO <moisespreto27@gmail.com>
2022-11-25 17:11:42 -0300

    Preparação de ambiente

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>AFF</title>
+    
+</head>
+<body>
+    <h3>Index</h3>
+</body>
+</html>
